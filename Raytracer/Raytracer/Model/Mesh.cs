20565ade3786812d633f395471145c03268ba651
YuryStrelkov <ghost_strelkov@mail.ru>
2020-06-25 12:41:08 +0400

    +

---
+++ Raytracer/Raytracer/Model/Mesh.cs
@@ -0,0 +1,168 @@
+﻿using OpenTK;
+using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Threading.Tasks;
+using static Raytracer.Model.ObjMeshReader;
+
+namespace Raytracer.Model
+{
+    [Serializable]
+    [StructLayout(LayoutKind.Sequential)]
+    public struct Vertex
+    {
+        public Vector4 Position;
+
+        public Vector4 Normal;
+
+        public Vector4 Tangent;
+
+        public Vector2 UV;
+
+        public Vertex(Vector4 _Position, Vector4 _Normal, Vector4 _Tangent, Vector2 _UV)
+        {
+            Position = _Position;
+            Normal = _Normal;
+            Tangent = _Tangent;
+            UV = _UV;
+        }
+
+        public new int GetHashCode()
+        {
+            return Position.GetHashCode();
+        }
+    }
+
+    [Serializable]
+    [StructLayout(LayoutKind.Sequential)]
+    public struct Face
+    {
+        public  int P1 { get; private set; }
+
+        public  int P2 { get; private set; }
+
+        public  int P3 { get; private set; }
+
+        public  int MatID { get; set; }
+
+        public void InvertOrder()
+        {
+            int p = P1;
+
+            P1 = P3;
+
+            P3 = p;  
+        }
+
+        public Face(int p1, int p2, int p3)
+       {
+            P1 = p1;
+
+            P2 = p2;
+
+            P3 = p3;
+
+            MatID = -1;
+       }
+
+        public Face(int p1, int p2, int p3,int mat)
+        {
+            P1 = p1;
+
+            P2 = p2;
+
+            P3 = p3;
+
+            MatID = mat;
+        }
+    }
+
+    public class Mesh
+    {
+        private object mutex;
+
+        Dictionary<int,Vertex> Vertices;
+
+        List<Face> Faces;
+
+        private void LoadModel(string src)
+        {
+            RawMesh mesh = ReadObj(src);
+            
+            Parallel.ForEach(mesh.Faces, (KVpair) => 
+            {
+                foreach (ObjFace face in mesh.Faces[KVpair.Key])
+                {
+                    Vertex v_0 = new Vertex(mesh.Positons[face.V1.positionID], mesh.Normals[face.V1.normalID], Vector4.Zero, mesh.Uvs[face.V1.UVID]);
+                    Vertex v_1 = new Vertex(mesh.Positons[face.V2.positionID], mesh.Normals[face.V2.normalID], Vector4.Zero, mesh.Uvs[face.V2.UVID]);
+                    Vertex v_2 = new Vertex(mesh.Positons[face.V3.positionID], mesh.Normals[face.V3.normalID], Vector4.Zero, mesh.Uvs[face.V3.UVID]);
+
+                    Vector3 deltaPos1 = v_1.Position.Xyz - v_0.Position.Xyz;
+                    Vector3 deltaPos2 = v_2.Position.Xyz - v_0.Position.Xyz;
+
+                    Vector2 deltaUV1 = v_1.UV - v_0.UV;
+                    Vector2 deltaUV2 = v_2.UV - v_0.UV;
+
+                    float dividend = (deltaUV1.X * deltaUV2.Y - deltaUV1.Y * deltaUV2.X);
+
+                    dividend = dividend == 0 ? 0.0f : 1.0f / dividend;
+
+                    Vector3 tangent = (deltaPos1 * deltaUV2.Y - deltaPos2 * deltaUV1.Y) * dividend;
+
+                    v_0.Tangent = new Vector4(tangent.X, tangent.Y, tangent.Z, 0);
+                    v_1.Tangent = v_0.Tangent;
+                    v_2.Tangent = v_0.Tangent;
+
+                    AppendFace(v_0, v_1, v_2, face.MatID);
+                };
+            }
+            );
+ 
+        }
+
+        private void BuildModelTree()
+        {
+
+        }
+
+        public void AppendFace(Vertex v1, Vertex v2, Vertex v3)
+        {
+            lock (mutex)
+            {
+                Faces.Add(new Face(AppendVertex(v1), AppendVertex(v2), AppendVertex(v3)));
+            }
+        }
+
+        private void AppendFace(Vertex v1, Vertex v2, Vertex v3, int matID)
+        {
+            lock (mutex)
+            {
+                Faces.Add(new Face(AppendVertex(v1), AppendVertex(v2), AppendVertex(v3), matID));
+            }
+        }
+
+        private int AppendVertex(Vertex v)
+        {
+            int vertexHashCode = v.GetHashCode();
+
+            if (!Vertices.ContainsKey(vertexHashCode))
+            {
+                Vertices.Add(vertexHashCode, v);
+            }
+            return vertexHashCode;
+        }
+
+        public Mesh(string src)
+        {
+            Vertices = new Dictionary<int, Vertex>();
+
+            Faces= new List<Face>();
+
+            mutex = new object();
+
+            LoadModel(src);
+
+            BuildModelTree();
+        }
+    }
+}
---
+++ Raytracer/Raytracer/Model/ObjMeshReader.cs
@@ -0,0 +1,311 @@
+﻿using OpenTK;
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Raytracer.Model
+{
+    public static class ObjMeshReader
+    {
+        public struct VertexComponentsIndeses
+        {
+            public int positionID;
+
+            public int normalID;
+
+            public int UVID;
+
+            public VertexComponentsIndeses(int p, int n, int uv)
+            {
+                positionID = p;
+                normalID = n;
+                UVID = uv;
+            }
+        }
+
+        public struct ObjFace
+        {
+            public VertexComponentsIndeses V1;
+
+            public VertexComponentsIndeses V2;
+
+            public VertexComponentsIndeses V3;
+
+            public int MatID;
+
+            public ObjFace(VertexComponentsIndeses v1, VertexComponentsIndeses v2, VertexComponentsIndeses v3,int _MatID)
+            {
+                V1 = v1;
+                V2 = v2;
+                V3 = v3;
+                MatID = _MatID;
+            }
+        }
+
+        public class RawMesh
+        {
+            public List<Vector4> Positons;/// = new List<Vector4>();
+            public List<Vector4> Normals;/// = new List<Vector4>();
+            public List<Vector4> Tangents;// = new List<Vector4>();
+            public List<Vector2> Uvs;/// = new List<Vector2>();
+            public Dictionary<int, List<ObjFace>> Faces { get; private set; }/// = new List<ObjFace>();
+            public Dictionary<int, MeshMaterial> Materials { get; private set; }/// = new List<ObjFace>();
+
+            public void AddMaterial(int matID,MeshMaterial mat)
+            {
+                if (!Materials.ContainsKey(matID))
+                {
+                    Materials.Add(matID, mat);
+                }
+            }
+
+            public MeshMaterial GetMaterial(int matID)
+            {
+                return Materials[matID];
+            }
+
+            public void AddFace(int geomety, ObjFace face)
+            {
+                face.V1.positionID += Positons.Count;
+                face.V1.normalID += Normals.Count;
+                face.V1.UVID += Uvs.Count;
+
+                face.V2.positionID += Positons.Count;
+                face.V2.normalID += Normals.Count;
+                face.V2.UVID += Uvs.Count;
+
+                face.V3.positionID += Positons.Count;
+                face.V3.normalID += Normals.Count;
+                face.V3.UVID += Uvs.Count;
+
+                if (Faces.ContainsKey(geomety))
+                {
+                    Faces[geomety].Add(face);
+                    return;
+                }
+
+                Faces.Add(geomety, new List<ObjFace>());
+
+                Faces[geomety].Add(face);
+            }
+
+            public RawMesh()
+            {
+                Positons = new List<Vector4>();
+                Normals = new List<Vector4>();
+                Tangents = new List<Vector4>();
+                Uvs = new List<Vector2>();
+                Faces = new Dictionary<int, List<ObjFace>>();
+                Materials = new Dictionary<int, MeshMaterial>();
+            }
+        }
+
+        public class MeshMaterial
+        {
+            public string MatName;
+            public float Ns;
+            public float D;
+            public float Tr;
+            public float Illum;
+            public Vector3 Tf;
+            public Vector3 Ka;
+            public Vector3 Kd;
+            public Vector3 Ks;
+       }
+
+        public static RawMesh ReadObj(string path)
+        {
+            RawMesh meshes = new RawMesh();
+            int matID = -1;
+            string[] Lines;
+            try
+            {
+                Lines = File.ReadAllLines(path);
+
+                string geometryName = "";
+
+                for (int i = 0; i < Lines.Length; i++)
+                {
+      
+
+                    if (Lines[i].StartsWith("#"))
+                    {
+                        continue;
+                    }
+
+                    if (Lines[i].StartsWith("mtllib"))
+                    {
+                        ReadMaterials(Lines[i].Split(' ')[1], ref meshes);
+                    }
+
+                    if (Lines[i].StartsWith("g"))
+                    {
+                        geometryName = Lines[i].Split(' ')[1];
+                    }
+
+                    if (Lines[i].StartsWith("usemtl"))
+                    {
+                        matID = ReadObjMatID(ref Lines[i]);
+                    }
+
+                    if (Lines[i].StartsWith("v"))
+                    {
+                        meshes.Positons.Add(ReadObjVector4(ref Lines[i]));
+                        continue;
+                    }
+
+                    if (Lines[i].StartsWith("vn"))
+                    {
+                        meshes.Normals.Add(ReadObjVector4(ref Lines[i]));
+                        continue;
+                    }
+
+                    if (Lines[i].StartsWith("vt"))
+                    {
+                        meshes.Uvs.Add(ReadObjVector2(ref Lines[i]));
+                        continue;
+                    }
+                    if (Lines[i].StartsWith("f"))
+                    {
+                        meshes.AddFace(geometryName.GetHashCode(), ReadObjFace(ref Lines[i], matID));
+                        continue;
+                    }
+                }
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("File not found...");
+            }
+            return meshes;
+        }
+
+        private static void ReadMaterials(string path, ref RawMesh mesh)
+        {
+            try
+            {
+                string[] Lines = File.ReadAllLines(path);
+
+                int matName = -1;
+
+                MeshMaterial mat = new MeshMaterial();
+
+                string line = "";
+
+                for (int i = 0; i < Lines.Length; i++)
+                {
+                    line = Lines[i].Trim();
+
+                    if (line.StartsWith("newmtl"))
+                    {
+                        matName = line.Split(' ')[1].GetHashCode();
+                        mesh.AddMaterial(matName, new MeshMaterial());
+                        continue;
+                    }
+
+                    if (line.StartsWith("Ns"))
+                    {
+                        mesh.GetMaterial(matName).Ns = ReadObjFloat(ref line);
+                        continue;
+                    }
+
+                    if (line.StartsWith("d"))
+                    {
+                        mesh.GetMaterial(matName).D = ReadObjFloat(ref line);
+                        continue;
+                    }
+
+                    if (line.StartsWith("Tr"))
+                    {
+                        mesh.GetMaterial(matName).Tr = ReadObjFloat(ref line);
+                        continue;
+                    }
+
+                    if (line.StartsWith("Tf"))
+                    {
+                        mesh.GetMaterial(matName).Tf = ReadObjVector3(ref line);
+                        continue;
+                    }
+
+                    if (line.StartsWith("illum"))
+                    {
+                        mesh.GetMaterial(matName).Illum = ReadObjFloat(ref line);
+                        continue;
+                    }
+
+                    if (line.StartsWith("Ka"))
+                    {
+                        mesh.GetMaterial(matName).Ka = ReadObjVector3(ref line);
+                        continue;
+                    }
+
+                    if (line.StartsWith("Kd"))
+                    {
+                        mesh.GetMaterial(matName).Kd = ReadObjVector3(ref line);
+                        continue;
+                    }
+
+                    if (line.StartsWith("Ks"))
+                    {
+                        mesh.GetMaterial(matName).Ks = ReadObjVector3(ref line);
+                        continue;
+                    }
+                }
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("no material found for " + path.Split('.')[0]);
+            }
+        }
+
+        private static Vector4 ReadObjVector4(ref string line)
+        {
+            string[] lineSplit = line.Split(' ');
+            return new Vector4(float.Parse(lineSplit[1]), float.Parse(lineSplit[2]), float.Parse(lineSplit[3]),1);
+        }
+        
+        private static Vector3 ReadObjVector3(ref string line)
+        {
+            string[] lineSplit = line.Split(' ');
+            return new Vector3(float.Parse(lineSplit[1]), float.Parse(lineSplit[2]), float.Parse(lineSplit[3]));
+        }
+
+        private static Vector2 ReadObjVector2(ref string line)
+        {
+            string[] lineSplit = line.Split(' ');
+            return new Vector2(float.Parse(lineSplit[1]), float.Parse(lineSplit[2]));
+        }
+
+        private static float ReadObjFloat(ref string line)
+        {
+            string[] lineSplit = line.Split(' ');
+            return float.Parse(lineSplit[1]);
+        }
+
+        private static int ReadObjMatID(ref string line)
+        {
+            string[] lineSplit = line.Split(' ');
+            return lineSplit[1].GetHashCode();
+        }
+
+        private static ObjFace ReadObjFace(ref string line, int matID)
+        {
+            string[] lineSplit = line.Split(' ');
+
+            string[] tmp = lineSplit[1].Split('/');
+
+            VertexComponentsIndeses V1 = new VertexComponentsIndeses(int.Parse(tmp[0]), int.Parse(tmp[1]), int.Parse(tmp[2]));
+
+            tmp = lineSplit[2].Split('/');
+
+            VertexComponentsIndeses V2 = new VertexComponentsIndeses(int.Parse(tmp[0]), int.Parse(tmp[1]), int.Parse(tmp[2]));
+
+            tmp = lineSplit[3].Split('/');
+
+            VertexComponentsIndeses V3 = new VertexComponentsIndeses(int.Parse(tmp[0]), int.Parse(tmp[1]), int.Parse(tmp[2]));
+
+            return new ObjFace(V1,V2,V3,matID);
+        }
+
+    }
+}
